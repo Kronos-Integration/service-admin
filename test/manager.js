bb83e7e53ec5bb260c579a1f041c868fd62ae369
Markus Felten <Markus.Felten@gmx.de>
2015-04-23 21:27:55 +0200

    promise integration

---
+++ test/manager.js
@@ -0,0 +1,45 @@
+/* global describe, it*/
+/* jslint node: true, esnext: true */
+
+"use strict";
+
+const chai = require('chai');
+chai.use(require("chai-as-promised"));
+const assert = chai.assert;
+const expect = chai.expect;
+
+const kronos = require('../lib/manager.js');
+
+describe('service manager', function () {
+	const flowDecl = {
+		"myFlow": {
+			"steps": {
+				"s1": {
+					"type": "copy",
+					"config": {
+						"key1": "value1"
+					},
+					"endpoints": {
+						"in": "stdin",
+						"out": function* () {
+							do {
+								let request =
+									yield;
+							} while (true);
+						}
+					}
+				}
+			}
+		}
+	};
+
+	describe('buildin step implementations', function () {
+		it('should be present', function (done) {
+			kronos.manager().then(function (myManager) {
+				const c = kronos.stepImplementation.implementations.copy;
+				expect(c.name).to.equal('copy');
+				done();
+			});
+		});
+	});
+});
