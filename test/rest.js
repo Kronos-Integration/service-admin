adcf094a987394ad458a13ed71cf32d5abf8ec72
Markus Felten <Markus.Felten@gmx.de>
2015-08-20 00:14:04 +0200

    style: playing around a little bit no real work

--- test/rest.js
+++ test/rest.js
@@ -157,6 +157,7 @@ describe('service manager REST', function () {
           .expect('Content-Type', /json/)
           .expect(200)
           .expect(function (res) {
+            console.log(res.text);
             //const response = JSON.parse(res.text);
             //if (response.name !== 'flow1') throw Error("flow flow1 missing");
           })
