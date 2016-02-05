57e83baa8741b8b42fae49f748e6c401614a8303
Markus Felten <Markus.Felten@gmx.de>
2016-02-05 12:22:16 +0100

    test: wip

--- tests/admin_test.js
+++ tests/admin_test.js
@@ -11,7 +11,7 @@ const chai = require('chai'),
   kronos = require('kronos-service-manager'),
   admin = require('../lib/adminService');
 
-describe('service manager admin', () => {
+describe('service admin', () => {
   const flowDecl = {
     "name": "flow1",
     "type": "kronos-flow",
@@ -39,118 +39,113 @@ describe('service manager admin', () => {
   }, [require('kronos-flow'),
     require('kronos-flow-control-step'),
     require('kronos-step-stdio'),
-    require('kronos-http-routing-step'),
-    admin
+    require('kronos-http-routing-step')
   ]);
 
-  describe('flows', () => {
-    it('GET /flows', () => {
-      return myManager.then(manager => {
-        console.log(`${Object.keys(manager.services)}`);
+  describe('register admin service', () =>
+    myManager.then(manager =>
+      admin.registerWithManager(manager).then(() => {
+        it('has admin service', () => assert.equal(manager.services.admin.name, 'admin'));
 
-        const as = manager.services.admin;
+        return manager.services.admin.start().then(() => {
+          console.log(`*** STATE ${manager.services.admin.state}`);
+          it('admin service is running', () => assert.equal(manager.services.admin.state, 'running'));
+        }).catch(e => {
+          console.log(e);
+        });
+      })
+    )
+  );
 
-        try {
-          request(as.server.listen())
-            .get('/flows')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(res => {
-              const response = JSON.parse(res.text);
-              console.log(`RES: ${JSON.stringify(response)}`);
-              if (response[1].url !== 'flow1') throw Error("flow missing");
-            })
-            .expect(200)
-            /*.end(done) */
-          ;
-        } catch (e) {
-          console.error(e);
-        }
-      } /*, done*/ );
+  describe('http', () => {
+    xit('GET /flows', () => {
+      return myManager.then(manager =>
+        request(manager.services.admin.server.listen())
+        .get('/flows')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(res => {
+          const response = JSON.parse(res.text);
+          console.log(`RES: ${JSON.stringify(response)}`);
+          if (response[1].url !== 'flow1') throw Error("flow missing");
+        })
+        .expect(200)
+        .end());
     });
-    xit('GET /flows/flow1', done => {
-      myManager.then(manager => {
-        request(as.server.listen())
-          .get('/flows/flow1')
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .expect(res => {
-            //console.log(`reponse: ${res.text}`);
-            const response = JSON.parse(res.text);
-            if (response.name !== 'flow1') throw Error("flow flow1 missing");
-          })
-          .end(done);
-      }, done);
-    });
+    xit('GET /flows/flow1', () =>
+      myManager.then(manager =>
+        request(manager.services.admin.server.listen())
+        .get('/flows/flow1')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(res => {
+          //console.log(`reponse: ${res.text}`);
+          const response = JSON.parse(res.text);
+          if (response.name !== 'flow1') throw Error("flow flow1 missing");
+        })
+        .end()
+      )
+    );
 
-    xit('DELETE /flows/flow1', done => {
-      myManager.then(manager => {
-        request(as.server.listen())
-          .delete('/flows/flow1')
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .expect(res => {
-            const response = JSON.parse(res.text);
-            if (Object.keys(response).length > 0) throw Error("delete error");
-          })
-          /*
-                    .get('/flows')
-                    .expect(200)
-                    .expect(function(res) {
-                      const response = JSON.parse(res.text);
-                      console.log(`RES: ${JSON.stringify(response)}`);
-                    })
-          */
-          .end(done);
-      }, done);
-    });
+    xit('DELETE /flows/flow1', () =>
+      myManager.then(manager =>
+        request(manager.services.admin.server.listen())
+        .delete('/flows/flow1')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(res => {
+          const response = JSON.parse(res.text);
+          if (Object.keys(response).length > 0) throw Error("delete error");
+        })
+        .end()
+      ));
 
-    xit('POST /flows', done => {
-      myManager.then(manager => {
-        request(as.server.listen())
-          .post('/flows')
-          .send({
-            "name": "a",
-            "type": "kronos-flow",
-            "steps": {}
-          })
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .expect(res => {
-            console.log(res.text);
-            //const response = JSON.parse(res.text);
-            //if (response.name !== 'flow1') throw Error("flow flow1 missing");
-          })
-          .end(done);
-      }, done);
-    });
+    xit('POST /flows', () =>
+      myManager.then(manager =>
+        request(manager.services.admin.server.listen())
+        .post('/flows')
+        .send({
+          "name": "a",
+          "type": "kronos-flow",
+          "steps": {}
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(res => {
+          console.log(res.text);
+          //const response = JSON.parse(res.text);
+          //if (response.name !== 'flow1') throw Error("flow flow1 missing");
+        })
+        .end()
+      )
+    );
 
-    xit('POST /flows with error', done => {
-      myManager.then(manager => {
-        request(as.server.listen())
-          .post('/flows')
-          .send({
-            "name": "a",
-            "type": "kronos-flow",
-            "steps": {
-              "s1": {
-                "type": "no-such-type"
-              }
+    xit('POST /flows with error', () =>
+      myManager.then(manager =>
+        request(manager.services.admin.server.listen())
+        .post('/flows')
+        .send({
+          "name": "a",
+          "type": "kronos-flow",
+          "steps": {
+            "s1": {
+              "type": "no-such-type"
             }
-          })
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .expect(res => {
-            console.log("RES:" + res.text);
-            //const response = JSON.parse(res.text);
-            //if (response.name !== 'flow1') throw Error("flow flow1 missing");
-          })
-          .end(done);
-      }, done);
-    });
+          }
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(res => {
+          console.log("RES:" + res.text);
+          //const response = JSON.parse(res.text);
+          //if (response.name !== 'flow1') throw Error("flow flow1 missing");
+        })
+        .end()
+      )
+    );
   });
 });
