8d8858942a01f3705c49e78b8dfba4ba4d6911f5
Markus Felten <Markus.Felten@gmx.de>
2019-11-25 14:23:04 +0100

    fix: remove outdated kronos api
    BREAKING CHANGE: fresh start minimal kronos admin

--- src/service-admin.mjs
+++ src/service-admin.mjs
@@ -1,15 +1,9 @@
-const path = require('path'),
-  process = require('process');
-
-import { sshServer } from './ssh.mjs';
-import { mergeAttributes, createAttributes } from 'model-attributes';
-import { ReceiveEndpoint } from '@kronos-integration/endpoint';
-import { Service, defineServiceConsumerProperties } from '@kronos-integration/service';
+import { Service } from '@kronos-integration/service';
 
 /**
  * Kronos administration service
  */
-export default class ServiceAdmin extends Service {
+export class ServiceAdmin extends Service {
   /**
    * @return {string} 'admin'
    */
@@ -17,88 +11,19 @@ export default class ServiceAdmin extends Service {
     return 'admin';
   }
 
-  static get configurationAttributes() {
-    return mergeAttributes(
-      createAttributes({
-        ssh: {
-          description: 'ssh admin interface',
-          attributes: {
-            port: {
-              description: 'listen port',
-              needsRestart: true,
-              type: 'ip-port'
-            },
-            hostKeys: {
-              description: 'server ssh private host key(s)',
-              type: 'blob',
-              needsRestart: true
-            }
-          }
-        }
-      }),
-      Service.configurationAttributes
-    );
-  }
-
-  constructor(...args) {
-    super(...args);
-
-    this.addEndpoint(
-      new ReceiveEndpoint('software', this)
-    ).receive = async request => {
-      return {
-        versions: process.versions,
-        platform: process.platform
-      };
+  static get endpoints() {
+    return {
+      ...super.endpoints,
+      services: {
+        default: true,
+        receive: "services"
+      }
     };
   }
 
-  /**
-   * Always start immediate
-   * @return {boolean} true
-   */
-  get autostart() {
-    return true;
-  }
-
-  async _start() {
-    await super._start();
-
-    await defineServiceConsumerProperties(
-      this,
-      {
-        listener: {
-          name: 'koa-admin',
-          type: 'koa'
-        }
-      },
-      this.owner,
-      true
-    );
-
-    if (this.ssh !== undefined) {
-      this.sshServer = sshServer(this, this.ssh);
-    }
-
-    return this.owner.loadFlowFromFile(
-      path.join(__dirname, '..', 'admin.flow')
-    );
-  }
-
-  async _stop() {
-    await super._stop();
-
-    if (this.sshServer) {
-      return new Promise((fullfill, reject) => {
-        this.sshServer.close(err => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          delete this.sshServer;
-          fullfill();
-        });
-      });
-    }
+  async services(params) {
+    return this.owner.services;
   }
 }
+
+export default ServiceAdmin;
--- tests/admin-test.mjs
+++ tests/admin-test.mjs
@@ -1,176 +1,33 @@
-import { registerWithManager } from '../src/service-admin';
-import test from 'ava';
+import test from "ava";
+import { StandaloneServiceProvider } from "@kronos-integration/service";
+import { ServiceAdmin } from "../src/service-admin.mjs";
 
-const { manager } = require('kronos-service-manager');
+const config = {
+  admin: {
+    type: ServiceAdmin
+  }
+};
 
-async function makeManager() {
-  const flowDecl = {
-    name: 'a',
-    type: 'kronos-flow',
-    steps: {
-      s0: {
-        type: 'kronos-stdin',
-        endpoints: {
-          out: 's1.in'
-        }
-      },
-      s1: {
-        type: 'kronos-stdout'
-      }
-    }
-  };
+test("service-admin", async t => {
+  const sp = new StandaloneServiceProvider();
+  const [admin] = await sp.declareServices(config);
+  await admin.start();
 
-  return manager(
-    {
-      kronos: {
-        logLevel: 'trace'
-      },
-      'koa-admin': {
-        logLevel: 'trace'
-      },
-      admin: {
-        logLevel: 'trace'
-      }
+  t.is(admin.state, "running");
+
+  let response = await admin.endpoints.services.receive(undefined);
+
+  response = JSON.parse(JSON.stringify(response));
+
+  t.log(response);
+
+  t.deepEqual(response, {
+    StandaloneServiceProvider: {
+      name: "StandaloneServiceProvider",
+      type: "StandaloneServiceProvider"
     },
-    [
-      require('kronos-service-registry'),
-      require('kronos-service-koa'),
-      require('kronos-service-health-check'),
-      require('kronos-interceptor-http-request'),
-      require('kronos-interceptor-decode-json'),
-      require('kronos-flow')
-
-      /*
-      require('kronos-flow-control-step'),
-      require('kronos-step-aggregate'),
-      require('kronos-step-stdio'),
-      require('kronos-http-routing-step')*/
-    ]
-  );
-}
-
-test('service admin register admin service', async t => {
-  const manager = await makeManager();
-
-  await registerWithManager(manager);
-
-  await manager.services.admin.start();
-
-  t.is(manager.services.admin.state, 'running');
-});
-
-/*
-describe('service admin', () => {
-  describe('http', () => {
-    it('GET /localnode/software', () =>
-      myManager.then(manager => {
-        const admin = manager.services['koa-admin'];
-        const app = admin.server.listen();
-        return request(app)
-          .get('/api/localnode/software')
-          .then(res => {
-            expect(res).to.have.status(200);
-          })
-          .catch(err => {
-            throw err;
-          });
-      }));
-
-    it('PUT /localnode/flow', () =>
-      myManager.then(manager => {
-        const admin = manager.services['koa-admin'];
-        const app = admin.server.listen();
-        return request(app)
-          .put('/api/localnode/flow/')
-          .send(
-            JSON.stringify({
-              name: 'a',
-              type: 'kronos-flow',
-              steps: {}
-            })
-          )
-          .then(res => {
-            expect(res).to.have.status(200);
-          });
-      }));
-
-    it('GET /localnode/flow', () =>
-      myManager.then(manager => {
-        manager.registerFlow(
-          manager.createStepInstanceFromConfig(flowDecl, manager)
-        );
-        const admin = manager.services['koa-admin'];
-        const app = admin.server.listen();
-        return request(app)
-          .get('/api/localnode/flow')
-          .then(res => {
-            expect(res).to.have.status(200);
-          });
-      }));
-
-    it('GET /localnode/flow/a', () =>
-      myManager.then(manager => {
-        const admin = manager.services['koa-admin'];
-        const app = admin.server.listen();
-
-        return request(app)
-          .get('/api/localnode/flow/a')
-          .then(res => {
-            expect(res).to.have.status(200);
-          });
-      }));
-
-    it('DELETE /localnode/flow/a', () =>
-      myManager.then(manager => {
-        const admin = manager.services['koa-admin'];
-        const app = admin.server.listen();
-        return request(app)
-          .delete('/api/localnode/flow/a')
-          .then(res => {
-            expect(res).to.have.status(200);
-          });
-      }));
-
-    xit('PUT /localnode/flow with error', () => {
-      try {
-        return myManager
-          .then(manager => {
-            const admin = manager.services['koa-admin'];
-            const app = admin.server.listen();
-            return request(app)
-              .put('/api/localnode/flow/')
-              .send(
-                JSON.stringify({
-                  name: 'a',
-                  type: 'kronos-flow',
-                  steps: {
-                    s1: {
-                      type: 'no-such-type'
-                    }
-                  }
-                })
-              )
-              .then(
-                res => {
-                  console.log(`AA ${res}`);
-                  expect(res).to.have.status(200);
-                },
-                rej => {
-                  console.log(`REJ: ${rej}`);
-                }
-              )
-              .catch(e => {
-                console.log(`CATCH: ${e}`);
-              });
-          })
-          .catch(e => {
-            console.log(`CATCH 2: ${e}`);
-          });
-      } catch (e) {
-        console.log(`ex: ${e}`);
-      }
-    });
+    admin: { name: "admin", type: "admin" },
+    config: { name: "config", type: "config" },
+    logger: { name: "logger", type: "logger" }
   });
 });
-
-*/
