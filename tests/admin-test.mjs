96766087d068277094f9b4311f8c0e2c2466e799
Markus Felten <Markus.Felten@gmx.de>
2019-12-12 15:18:49 +0100

    test: fix typos

--- tests/admin-test.mjs
+++ tests/admin-test.mjs
@@ -22,9 +22,9 @@ test("service-admin", async t => {
   t.log(response);
 
   t.deepEqual(response, {
-    'stantalone-provider': {
-      name: "stantalone-provider",
-      type: "stantalone-provider"
+    'standalone-provider': {
+      name: "standalone-provider",
+      type: "standalone-provider"
     },
     admin: { name: "admin", type: "admin" },
     config: { name: "config", type: "config" },
