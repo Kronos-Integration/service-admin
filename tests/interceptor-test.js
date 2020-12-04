31985ee0434aacb658da506a4fe36d9431c4150d
Markus Felten <Markus.Felten@gmx.de>
2020-12-04 03:30:44 +0100

    add missing tests/interceptor-test.js from template

---
+++ tests/interceptor-test.js
@@ -0,0 +1,76 @@
+/* global describe, it, xit */
+/* jslint node: true, esnext: true */
+
+"use strict";
+
+/*
+ * Just test if the message will be passed through the interceptor
+ */
+
+const chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect,
+  should = chai.should(),
+  InterceptorUnderTest = require('../index').Interceptor,
+  MockReceiveInterceptor = require('kronos-test-interceptor').MockReceiveInterceptor;
+
+
+const stepMock = {
+  "name": "dummy step name",
+  "type": "dummy step type"
+};
+
+const checkProperties = {
+  "config": {
+    "namus": {
+      "fieldType": {
+        "type": "boolean",
+      },
+      "defaultValue": "false",
+      "mandatory": true,
+      "severity": "abort_file"
+    }
+  }
+};
+
+describe('Interceptor test', function () {
+
+  it('Create', function () {
+    const endpoint = {
+      "owner": stepMock,
+      "name": "gumboIn"
+    };
+    const messageHandler = new InterceptorUnderTest(checkProperties, endpoint);
+    assert.ok(messageHandler);
+  });
+
+  it('Send message', function (done) {
+    const endpoint = {
+      "owner": stepMock,
+      "name": "gumboIn"
+    };
+
+    const sendMessage = {
+      "info": "first message"
+    };
+
+    const messageHandler = new InterceptorUnderTest(checkProperties, endpoint);
+    const mockReceive = new MockReceiveInterceptor(function (request, oldRequest) {
+
+      assert.ok(request);
+
+      assert.deepEqual(request, {
+        "info": "first message"
+      });
+      done();
+    });
+
+    messageHandler.connected = mockReceive;
+
+    messageHandler.receive(sendMessage);
+
+  });
+
+
+
+});
